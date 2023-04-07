b8233d217e75ba2ea046d5cce2613d51a83d4c70
yinchaogao <yinchaogao@deeproute.ai>
2023-04-07 12:14:26 +0800

    build examples

--- build.sh
+++ build.sh
@@ -17,6 +17,7 @@ build_type=Release
     -DLLVM_TARGETS_TO_BUILD='all' \
     -DLLVM_ENABLE_RTTI=ON \
     -DLLVM_INSTALL_UTILS=ON \
+    -DLLVM_BUILD_EXAMPLES=ON \
     -DMLIR_ENABLE_BINDINGS_PYTHON=ON \
     -DPython3_EXECUTABLE="$(which python3)" \
     && cmake --build build -j "$(nproc)" \
